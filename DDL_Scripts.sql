72a0644dd7ac8903f20c71355e3d27151787654d
Abhinav <gangurde.a@northeastern.edu>
2025-03-10 22:19:11 -0400

    Updated for datatypes

--- DDL_Scripts.sql
+++ DDL_Scripts.sql
@@ -1,10 +1,10 @@
-USE Team2_FinalProject_DMDD_v1;
+USE Team2_FinalProject_DMDD;
 
 -- Table for Admin
 DROP TABLE IF EXISTS [Admin]
 CREATE TABLE [Admin] (
     [ID] INT IDENTITY(1,1) NOT NULL,
-    [AdminID] AS 'ADMIN' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
+    [AdminID] AS 'ADMIN' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
     [FirstName] varchar(40) NOT NULL,
     [LastName] varchar(40),
     [Password] varchar(40) NOT NULL,
@@ -15,29 +15,28 @@ CREATE TABLE [Admin] (
 DROP TABLE IF EXISTS [RegisteredUsers];
 CREATE TABLE [RegisteredUsers] (
   [ID] INT IDENTITY(1,1) NOT NULL,
-  [UserID] AS 'R_USR' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
+  [UserID] AS 'R_USR' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
   [AdminID] varchar(10),
   [FirstName] varchar(10) NOT NULL,
   [LastName] varchar(50),
   [Password] varchar(40) NOT NULL,
   [EmailID] varchar(50) NOT NULL,
   [PhoneNumber] varchar(10) NOT NULL,
-  [Type] varchar(10) NOT NULL,
+  [Type] varchar(30) NOT NULL,
   CONSTRAINT CHK_UserType CHECK ([Type] IN ('Driver', 'Renter', 'Rider')),
   CONSTRAINT [PK_RegisteredUsers] PRIMARY KEY ([UserID]),
-  CONSTRAINT [FK_AdminID] FOREIGN KEY (AdminID) REFERENCES [Admin](AdminID),
-  CONSTRAINT [UQ_RegisteredUsers_EmailID] UNIQUE ([EmailID])
-  );
+  CONSTRAINT [FK_AdminID] FOREIGN KEY (AdminID) REFERENCES [Admin](AdminID) 
+);
 
 -- Table for Renter
 DROP TABLE IF EXISTS [Renter];
 CREATE TABLE [Renter] (
   [ID] INT IDENTITY(1,1) NOT NULL,
-  [RenterID] AS 'RENTR' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
+  [RenterID] AS 'RENTR' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
   [UserID] varchar(10) NOT NULL,
-  [TotalRentedCars] INT DEFAULT 0,
-  [TotalEarnings] DECIMAL(19,2) DEFAULT 0.00,
-  [TotalRentalTime] int DEFAULT 0,
+  [TotalRentedCars] decimal(10,2) NOT NULL DEFAULT 0,
+  [TotalEarnings] decimal(10,2) NOT NULL DEFAULT 0,
+  [TotalRentalTime] int NOT NULL DEFAULT 0,
   [CompanyName] varchar(30),
   CONSTRAINT [PK_RenterID] PRIMARY KEY ([RenterID]),
   CONSTRAINT [FK_RenterUserID] FOREIGN KEY ([UserID]) REFERENCES RegisteredUsers(UserID)
@@ -48,28 +47,25 @@ CREATE TABLE [Renter] (
 DROP TABLE IF EXISTS [Driver];
 CREATE TABLE [Driver] (
   [ID] INT IDENTITY(1,1) NOT NULL,
-  [DriverID] AS 'DRIVE' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
-  [UserID] varchar(10) NOT NULL,
-  [LicenseNo] varchar(10) NOT NULL,
-  [AvailabilityStatus] varchar(15) DEFAULT 'Available',
-  [TotalCompletedRides] INT DEFAULT 0,
-  [TotalEarnings] DECIMAL(19,2) NOT NULL DEFAULT 0.00,
-  [Rating] DECIMAL(3,1) NOT NULL DEFAULT 0.0,
-  CONSTRAINT [CHK_Rating] CHECK ([Rating] >= 0.0 AND [Rating] <= 5.0),
+  [DriverID] AS 'DRIVE' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
+  [UserID] varchar(10) NOT NULL REFERENCES RegisteredUsers(UserID),
+  [LicenseNo] varchar(10) NOT NULL DEFAULT 'NULL',
+  [AvailabilityStatus] varchar(15) NOT NULL DEFAULT 'Available',
+  [TotalCompletedRides] INT NOT NULL DEFAULT 0,
+  [TotalEarnings] INT NOT NULL DEFAULT 0,
+  [Rating] INT NOT NULL DEFAULT 0,
   CONSTRAINT [PK_DriverID] PRIMARY KEY ([DriverID]),
-  CONSTRAINT [CHK_AvailabilityStatus] CHECK ([AvailabilityStatus] IN ('In-Transit', 'Available', 'Out-of-Service')),
-  CONSTRAINT [UQ_Driver_LicenseNo] UNIQUE ([LicenseNo]),
-  CONSTRAINT [FK_Driver_UserID] FOREIGN KEY (UserID) REFERENCES RegisteredUsers(UserID),
+  CONSTRAINT [CHK_AvailabilityStatus] CHECK ([AvailabilityStatus] IN ('In-Transit', 'Available', 'Out-of-Service'))
 );
 
 -- Table for Rider
 DROP TABLE IF EXISTS [Rider];
 CREATE TABLE [Rider] (
   [ID] INT IDENTITY(1,1) NOT NULL,
-  [RiderID] AS 'RIDER' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
+  [RiderID] AS 'RIDER' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
   [UserID] varchar(10) NOT NULL REFERENCES RegisteredUsers(UserID),
-  [TotalPreviousRides] INT DEFAULT 0,
-  [AmountDue] decimal(19,2) DEFAULT 0,
+  [TotalPreviousRides] INT NOT NULL DEFAULT 0,
+  [AmountDue] decimal(10,2) NOT NULL DEFAULT 0,
   CONSTRAINT [PK_RiderID] PRIMARY KEY ([RiderID])
 );
 
@@ -78,12 +74,12 @@ CREATE TABLE [Rider] (
 DROP TABLE IF EXISTS [Car];
 CREATE TABLE [Car] (
   [ID] INT IDENTITY(1,1) NOT NULL,
-  [CarID] AS 'R_CAR' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
+  [CarID] AS 'R_CAR' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
   [CarType] varchar(20) NOT NULL,
   [CarStatus] varchar(15) NOT NULL,
-  [CarModel] varchar(30),
-  [CarYear] int,
-  [IsElectric] BIT DEFAULT 0,
+  [CarModel] varchar(30) NOT NULL,
+  [CarYear] int NOT NULL,
+  [IsElectric] BIT NOT NULL,
   [NumberOfSeats] AS (
     CASE 
       WHEN [CarType] IN ('Sedan','Hatchbacks') THEN 4
@@ -121,8 +117,8 @@ CREATE TABLE [Location] (
   [Latitude] DECIMAL(10,8) NOT NULL,
   [Longitude] DECIMAL(11,8) NOT NULL,
   [GeohashID] AS dbo.CalculateGeohash(Latitude, Longitude, 8) PERSISTED,
-  [Region] VARCHAR(50),
-  [County] VARCHAR(50),
+  [Region] VARCHAR(50) NOT NULL,
+  [County] VARCHAR(50) NOT NULL,
   [City] VARCHAR(50) NOT NULL,
   [State] VARCHAR(50) NOT NULL,
   CONSTRAINT [PK_LocationID] PRIMARY KEY ([GeohashID])
@@ -133,7 +129,7 @@ DROP TABLE IF EXISTS [DriverLocation];
 CREATE TABLE [DriverLocation] (
   [DriverID] VARCHAR(10) NOT NULL,
   [GeohashID] VARCHAR(12) NOT NULL,
-  [LastUpdated] DATETIME DEFAULT GETDATE(),
+  [LastUpdated] DATETIME NOT NULL DEFAULT GETDATE(),
   CONSTRAINT [PK_DriverLocation] PRIMARY KEY ([DriverID]),
   CONSTRAINT [FK_DriverLocation_Driver] FOREIGN KEY ([DriverID]) REFERENCES [Driver]([DriverID]),
   CONSTRAINT [FK_DriverLocation_Location] FOREIGN KEY ([GeohashID]) REFERENCES [Location]([GeohashID])
@@ -155,7 +151,7 @@ CREATE TABLE [Ratecard] (
 DROP TABLE IF EXISTS [TripRequest];
 CREATE TABLE [TripRequest] (
   [TripID] INT IDENTITY(1,1) NOT NULL,
-  [TripRequestID] AS 'TRIP_' + RIGHT('00000' + CAST(TripID AS CHAR(5)), 5) PERSISTED,
+  [TripRequestID] AS 'TRIP_' + RIGHT('00000' + CAST(TripID AS VARCHAR(5)), 5) PERSISTED,
   [RiderID] VARCHAR(10) NOT NULL,
   [PickupLatitude] DECIMAL(10,8) NOT NULL,
   [PickupLongitude] DECIMAL(11,8) NOT NULL,
@@ -163,7 +159,7 @@ CREATE TABLE [TripRequest] (
   [DropoffLatitude] DECIMAL(10,8) NOT NULL,
   [DropoffLongitude] DECIMAL(11,8) NOT NULL,
   [DropoffGeohashID] AS dbo.CalculateGeohash(DropoffLatitude, DropoffLongitude, 8) PERSISTED,
-  [RequestTime] DATETIME DEFAULT GETDATE(),
+  [RequestTime] DATETIME NOT NULL DEFAULT GETDATE(),
   [EstimatedDistance] AS dbo.CalculateDistance(PickupLatitude, PickupLongitude, DropoffLatitude, DropoffLongitude) PERSISTED,
   [State] AS dbo.GetStateFromCoordinates(PickupLatitude, PickupLongitude) PERSISTED,
   [EstimatedCost] AS dbo.CalculateTripCost(dbo.CalculateDistance(PickupLatitude, PickupLongitude, DropoffLatitude, DropoffLongitude)) PERSISTED,
@@ -181,10 +177,10 @@ CREATE TABLE [TripRequest] (
 DROP TABLE IF EXISTS [Invoice];
 CREATE TABLE [Invoice] (
   [ID] INT IDENTITY(1,1) NOT NULL,
-  [InvoiceID] AS 'INVO_' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
+  [InvoiceID] AS 'INVO_' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
   [TripRequestID] varchar(10) NOT NULL,
   [Distance] decimal(5,2) NOT NULL,
-  [Price] decimal(10,2) NOT NULL,
+  [Price] decimal(10,3) NOT NULL,
   CONSTRAINT [PK_Invoice] PRIMARY KEY ([InvoiceID]),
   CONSTRAINT [FK_Invoice] FOREIGN KEY ([TripRequestID]) REFERENCES TripRequest([TripRequestID])
 );
@@ -194,7 +190,7 @@ CREATE TABLE [Invoice] (
 DROP TABLE IF EXISTS [PaymentRequest];
 CREATE TABLE [PaymentRequest] (
   [ID] INT IDENTITY(1,1) NOT NULL,
-  [PaymentID] AS 'PAY_R' + RIGHT('00000' + CAST(ID AS CHAR(5)), 5) PERSISTED,
+  [PaymentID] AS 'PAY_R' + RIGHT('00000' + CAST(ID AS VARCHAR(5)), 5) PERSISTED,
   [InvoiceID] varchar(10) NOT NULL,
   [RiderID] varchar(10) NOT NULL,
   [MethodOfPayment] varchar(25) NOT NULL,
@@ -215,7 +211,7 @@ CREATE TABLE [Card] (
   [CardHolder] varchar(50) NOT NULL,
   [ExpiryDate] date NOT NULL,
   [CVV] varchar(4) NOT NULL,
-  [Type] varchar(20),
+  [Type] varchar(20) NOT NULL,
   CONSTRAINT [PK_Card] PRIMARY KEY ([PaymentID]),
   CONSTRAINT [FK_Card_PaymentRequest] FOREIGN KEY ([PaymentID]) REFERENCES [PaymentRequest]([PaymentID])
 );
@@ -224,9 +220,9 @@ CREATE TABLE [Card] (
 DROP TABLE IF EXISTS [OnlineWallet];
 CREATE TABLE [OnlineWallet] (
   [PaymentID] varchar(10) NOT NULL,
-  [WalletID] char(20) NOT NULL,
-  [WalletProvider] varchar(30),
-  [AccountEmail] varchar(50),
+  [WalletID] varchar(20) NOT NULL,
+  [WalletProvider] varchar(30) NOT NULL,
+  [AccountEmail] varchar(50) NOT NULL,
   CONSTRAINT [PK_OnlineWallet] PRIMARY KEY ([PaymentID]),
   CONSTRAINT [FK_OnlineWallet_PaymentRequest] FOREIGN KEY ([PaymentID]) REFERENCES [PaymentRequest]([PaymentID])
 );
